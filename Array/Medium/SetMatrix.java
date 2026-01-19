467c126af704e59833ab119f69084816f0d50feb
Malti Pathak <164329815+Maltipat@users.noreply.github.com>
2026-01-19 16:47:41 +0530

    Add files via upload

---
+++ Array/Medium/SetMatrix.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class SetMatrix {
+    public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[] row = new boolean[rows];
+        boolean[] col = new boolean[cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
